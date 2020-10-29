e84dd0e3dd1cb136ee8b171c5a04b311ab90bbf8
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-29 16:21:20 -0700

    add add.post to test post request

--- server.js
+++ server.js
@@ -2,4 +2,25 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('<h1>HEELLLLOO</h1>')
+    next()
+})
+
+app.get('/', (req, res) => {
+    res.send("root")
+})
+
+app.get('/profile', (req, res) => {
+    res.send("getting profile")
+})
+
+app.post('/profile', (req, res) => {
+    const user = {
+        name: 'Sally',
+        hobby: 'soccer'
+    }
+    res.send(user)
+})
+
 app.listen(3000);
